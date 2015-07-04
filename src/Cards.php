ab9aff9404bff95152f4bdb82f2f292a9c2dbd06
Toshimitsu Suzuki <sionaion3@gmail.com>
2015-07-04 17:30:02 +0900

    add Card class

---
+++ src/Cards.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Poker;
+
+/**
+ * カードの準備, 配布などをコントロールするクラス
+ */
+class Cards
+{
+    private $_deck; // 山札
+    private $_types = ['clover', 'diamond', 'heart', 'spade']; // カードの絵柄
+
+    public function __construct()
+    {
+        $this->init();
+    }
+
+    /**
+     * カード初期化
+     */
+    public function init()
+    {
+        // 山札を空にする
+        $this->_deck = [];
+
+        foreach ($this->_types as $type) {
+            for ($i = 1; $i <= 13; $i++) {
+                $this->_deck[] = [
+                    'type'   => $type,
+                    'number' => $i
+                ];
+            }
+        }
+
+        // カードシャッフル
+        shuffle($this->_deck);
+    }
+
+    /**
+     * 指定枚数を配る
+     *
+     * @param int $num 配布枚数
+     *
+     * @return array
+     */
+    public function deal($num)
+    {
+        if (count($this->_deck) === 0) {
+            return [];
+        }
+
+        if (count($this->_deck) >= $num) {
+            return array_splice($this->_deck, 0, $num);
+        }
+
+        $cards = $this->_deck;
+        $this->_deck = [];
+
+        return $cards;
+    }
+}
